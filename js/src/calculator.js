c5c736c8a4be846aa791b8fa3c4175f156feca67
James Pederson <james@jpederson.com>
2019-03-06 10:11:09 -0600

    Initial commit.

---
+++ js/src/calculator.js
@@ -0,0 +1,44 @@
+
+
+$(function(){
+
+	/*
+	$(".calculator.home").accrue({
+		mode: "compare",
+		response_output_div: ".result.home",
+		response_compare:"You Save: <strong>$%savings%</strong>",
+		error_text:"",
+	});
+	*/
+
+	$(".calculator.loan-auto").accrue({
+		mode: "compare",
+		response_output_div: ".result.auto",
+		response_compare:"You Save: <strong>$%savings%</strong>",
+		error_text:"",
+		callback:function(elem, data){
+			console.log( data );
+			if ( typeof( data.savings ) !== 'undefined' ) {
+				if ( data.savings > 950 ) {
+					$(elem).find('.result.auto').html('You Save: <strong>$950</strong>');
+				}
+			}
+		}
+	});
+
+	/*
+	$(".calculator.loan-personal").accrue({
+		mode: "compare",
+		response_output_div: ".result.personal",
+		response_compare:"You Save: <strong>$%savings%</strong>",
+		error_text:"",
+	});
+	*/
+
+	$(".numbers-only").keyup(function(){
+		var fixed=$(this).val().replace(/[^0-9.]/g,"");
+		$(this).val( fixed );
+	});
+
+});
+
